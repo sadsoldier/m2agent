c9a9a957751e57eaff0b86c08e9f714d695e6206
Oleg Borodin <onborodin@gmail.com>
2020-02-14 23:20:03 +0200

    add s2 List() and Delete()

--- clients/s2/s2.go
+++ clients/s2/s2.go
@@ -5,33 +5,177 @@
 package s2client
 
 import (
-    "net/url"
-    "crypto/tls"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "mime/multipart"
-    "net/http"
-    "os"
+    "time"
+    "strconv"
     "path/filepath"
+    "os"
+    "net/url"
+    "net/http"
+    "mime/multipart"
+    "io/ioutil"
+    "io"
+    "fmt"
     "errors"
+    "encoding/json"
+    "crypto/tls"
+    "bytes"
+    "log"
 )
 
-func Get(storeUri, filename, destDir string) (string, error) {
-    var err error
+type URI struct {
+    Scheme      string
+    //Username    string
+    //Password    string
+    Hostname    string
+    Userinfo    string
+    Port        int
+    Path        string
+}
 
-    resourse, err := url.Parse(storeUri)
+const (
+    bufferSize int64 = 1024 * 128
+    partSuffix string = ".part"
+    defaultPort int = 7001
+)
+
+func UriParse(compactURI string) (URI, error) {
+
+    var err error
+    var uri URI
+
+    parsedURI, err := url.Parse(compactURI)
     if err != nil {
-        return "", err
+        return uri, err
     }
 
-    scheme := resourse.Scheme
-    host := resourse.Host
+    uri.Scheme = parsedURI.Scheme
+    uri.Hostname = parsedURI.Host
 
-    userinfo := resourse.User.String()
-    bucket := resourse.Path
+    uri.Port = defaultPort
+    if len(parsedURI.Port()) != 0 {
+        uri.Port, err = strconv.Atoi(parsedURI.Port())
+        if err != nil {
+            return uri, err
+        }
+    }
 
-    s2uri := fmt.Sprintf("%s://%s@%s/api/v1/file/down/%s/%s", scheme, userinfo, host, bucket, filename)
+    //uri.Username = parsedURI.User.Username()
+    //password, exists := parsedURI.User.Password()
+    //if exists {
+        //uri.Password = password
+    //}
+
+    uri.Userinfo = parsedURI.User.String()
+    uri.Path = parsedURI.Path
+
+    return uri, nil
+}
+
+type File struct {
+    Name        string
+    Size        int64
+    ModTime     time.Time
+}
+
+type S2File struct {
+    Name        string      `json:"name"`
+    Size        int64       `json:"size"`
+    ModTime     string      `json:"modtime"`
+}
+
+type S2ListResponse struct {
+    Error       bool        `json:"error"`
+    Message     string      `json:"message"`
+    Result      []S2File    `json:"result"`
+}
+
+type ListForm struct {
+    Bucket      string  `json:"bucket"`
+    Pattern     string  `json:"pattern"`
+}
+
+type S2DeleteResponse struct {
+    Error       bool        `json:"error"`
+    Message     string      `json:"message"`
+}
+
+
+func List(storeURI string) (*[]File, error) {
+    var err error
+    var response S2ListResponse
+    var files []File
+
+    parsedURI, err := UriParse(storeURI)
+    if err != nil {
+        return &files, err
+    }
+
+    s2url := fmt.Sprintf("%s://%s@%s/api/v1/file/list",
+        parsedURI.Scheme,
+        parsedURI.Userinfo,
+        parsedURI.Hostname)
+
+    form := ListForm{
+        Bucket: parsedURI.Path,
+        Pattern: "*",
+    }
+
+    transCfg := &http.Transport{
+         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+    client := &http.Client{Transport: transCfg}
+
+    data, _ := json.Marshal(form)
+    reader := bytes.NewReader([]byte(data))
+
+    resp, err := client.Post(s2url, "application/json", reader)
+    if err != nil {
+        return &files, err
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return &files, err
+    }
+
+    err = json.Unmarshal(body, &response)
+    if err != nil {
+        return &files, err
+    }
+
+    if response.Error == true {
+        log.Println(response.Message)
+        return &files, errors.New(response.Message)
+    }
+
+    /* Map result to file array */
+    for _, item := range response.Result {
+        var file File
+        file.Name = item.Name
+        file.Size = item.Size
+        file.ModTime, _ = time.Parse(time.RFC3339, item.ModTime)
+        files = append(files, file)
+    }
+
+    return &files, nil
+}
+
+func Get(storeURI, filename, destDir string) (string, error) {
+    var err error
+    var destFilepath string
+
+    parsedURI, err := UriParse(storeURI)
+    if err != nil {
+        return destFilepath, err
+    }
+
+    s2uri := fmt.Sprintf("%s://%s@%s/api/v1/file/down/%s/%s",
+        parsedURI.Scheme,
+        parsedURI.Userinfo,
+        parsedURI.Hostname,
+        parsedURI.Path,
+        filename)
 
     transCfg := &http.Transport{
          TLSClientConfig: &tls.Config{ InsecureSkipVerify: true },
@@ -40,52 +184,49 @@ func Get(storeUri, filename, destDir string) (string, error) {
 
     response, err := client.Get(s2uri)
     if err != nil {
-        return "", err
+        return destFilepath, err
     }
     defer response.Body.Close()
 
     if response.StatusCode != http.StatusOK {
-        return "", errors.New(fmt.Sprintf("Wrong status code: %d", response.StatusCode))
+        return destFilepath, errors.New(fmt.Sprintf("Wrong status code: %d", response.StatusCode))
     }
 
-    file, err := ioutil.TempFile(destDir, filename + ".part.*")
+    file, err := ioutil.TempFile(destDir, filename + partSuffix)
     if err != nil {
-        return "", err
+        return destFilepath, err
     }
     defer file.Close()
 
-    buffer := make([]byte,  128 * 1024)
+    buffer := make([]byte,  bufferSize)
     _, err = io.CopyBuffer(file, response.Body, buffer)
     if err != nil {
         os.Remove(file.Name())
-        return "", err
+        return destFilepath, err
     }
-    resPath := filepath.Join(destDir, filename)
+    destFilepath = filepath.Join(destDir, filename)
 
-    err = os.Rename(file.Name(), resPath)
+    err = os.Rename(file.Name(), destFilepath)
     if err != nil {
         os.Remove(file.Name())
-        return "", err
+        return destFilepath, err
     }
 
-    return resPath, nil
+    return destFilepath, nil
 }
 
-func Put(storeUri, filename string) error {
+func Put(storeURI, filename string) error {
     var err error
 
-    resourse, err := url.Parse(storeUri)
+    parsedURI, err := UriParse(storeURI)
     if err != nil {
         return err
     }
 
-    scheme := resourse.Scheme
-    host := resourse.Host
-
-    userinfo := resourse.User.String()
-    bucket := resourse.Path
-
-    s2url := fmt.Sprintf("%s://%s@%s/api/v1/file/put", scheme, userinfo, host)
+    s2url := fmt.Sprintf("%s://%s@%s/api/v1/file/put",
+        parsedURI.Scheme,
+        parsedURI.Userinfo,
+        parsedURI.Hostname)
 
     pipeOut, pipeIn := io.Pipe()
     writer := multipart.NewWriter(pipeIn)
@@ -95,7 +236,7 @@ func Put(storeUri, filename string) error {
         defer writer.Close()
 
         _ = writer.WriteField("filename", filepath.Base(filename))
-        _ = writer.WriteField("bucket", bucket)
+        _ = writer.WriteField("bucket", parsedURI.Path)
 
         part, err := writer.CreateFormFile("file", filepath.Base(filename))
         if err != nil {
@@ -123,5 +264,63 @@ func Put(storeUri, filename string) error {
     if err != nil {
         return err
     }
+    return nil
+}
+
+
+type deleteForm struct {
+    Filename    string  `form:"filename" json:"filename" binding:"required" `
+    Bucketname  string  `form:"bucket"   json:"bucket"`
+}
+
+
+func Delete(storeURI, filename string) error {
+    var err error
+    var response S2DeleteResponse
+
+    parsedURI, err := UriParse(storeURI)
+    if err != nil {
+        return err
+    }
+
+    s2url := fmt.Sprintf("%s://%s@%s/api/v1/file/delete",
+        parsedURI.Scheme,
+        parsedURI.Userinfo,
+        parsedURI.Hostname)
+
+    form := deleteForm{
+        Bucketname: parsedURI.Path,
+        Filename: filename,
+    }
+
+    transCfg := &http.Transport{
+         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+    client := &http.Client{Transport: transCfg}
+
+    data, _ := json.Marshal(form)
+    reader := bytes.NewReader([]byte(data))
+
+    resp, err := client.Post(s2url, "application/json", reader)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return err
+    }
+
+    err = json.Unmarshal(body, &response)
+    if err != nil {
+        return err
+    }
+
+    if response.Error == true {
+        log.Println(response.Message)
+        return errors.New(response.Message)
+    }
+
     return nil
 }
--- clients/s2/s2_test.go
+++ clients/s2/s2_test.go
@@ -13,22 +13,51 @@ import (
 
 )
 
-func TestPutGet(t *testing.T) {
+const (
+    uri string = "https://user1:12345@localhost:7001/12345"
+    filename string = "test.bin"
+)
 
+func TestPut(t *testing.T) {
     buffer := make([]byte, 1024)
-    tmpname := "file.test"
-    ioutil.WriteFile(tmpname, buffer, 0666)
+    ioutil.WriteFile(filename, buffer, 0666)
 
-    resourse := "https://user1:12345@localhost:7001/tmp"
-    err := Put(resourse, tmpname)
+    err := Put(uri, filename)
     if err != nil {
         t.Error(err)
     }
-    cwd, _ := os.Getwd()
-    filename, err := Get(resourse, tmpname, cwd)
-    if err != nil {
-        t.Error(err)
-    }
-    fmt.Println(filename)
     os.Remove(filename)
+}
+
+func TestList1(t *testing.T) {
+    response, err := List(uri)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(*response)
+}
+
+func TestGet(t *testing.T) {
+    outname, err := Get(uri, filename, "/tmp")
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(outname)
+    os.Remove(outname)
+}
+
+
+func TestDelete(t *testing.T) {
+    err := Delete(uri, filename)
+    if err != nil {
+        t.Error(err)
+    }
+}
+
+func TestList2(t *testing.T) {
+    response, err := List(uri)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(*response)
 }
